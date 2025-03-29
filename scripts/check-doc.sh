4022c3686ef10807541d156091a9b368578afe7e
ZelionD <>
2025-03-29 04:19:26 +0200

    rebase

--- scripts/check-doc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-here="$(dirname "$0")"
-src_root="$(readlink -f "${here}/..")"
-cd "${src_root}"
-
-export RUSTDOCFLAGS="-D warnings"
-./cargo nightly hack doc --all-features
--- sdk/src/pubkey.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(feature = "full")]
-pub use solana_pubkey::new_rand;
-#[cfg(target_os = "solana")]
-pub use solana_pubkey::syscalls;
-pub use solana_pubkey::{
-    bytes_are_curve_point, ParsePubkeyError, Pubkey, PubkeyError, MAX_SEEDS, MAX_SEED_LEN,
-    PUBKEY_BYTES,
-};
